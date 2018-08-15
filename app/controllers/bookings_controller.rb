f553ec11908bf94a1e30d08fad08ac1b45253a9f
tania perales <tania.perales.s@gmail.com>
2018-08-15 12:52:57 +0900

    booking policy

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,17 +1,14 @@
 class BookingsController < ApplicationController
   before_action :set_booking, only: [:show, :destroy]
   def index
-    @bookings = Booking.all
-  end
-
-  def new
-    @booking = Booking.new
+    @bookings = policy_scope(Booking)
   end
 
   def create
     @car = Car.find(params[:car_id])
     @booking = Booking.new(booking_params)
     @booking.car = @car
+    authorize @booking
     @booking.user = current_user
     if @booking.save
       redirect_to booking_path(@booking)
@@ -26,15 +23,18 @@ class BookingsController < ApplicationController
   def destroy
     @booking.destroy
     redirect_to bookings_path
+
   end
 
   private
 
   def set_booking
     @booking = Booking.find(params[:id])
+    authorize @booking
   end
 
   def booking_params
     params.require(:booking).permit(:start_date, :end_date)
   end
+
 end
