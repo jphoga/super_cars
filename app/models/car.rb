b465ac68a0cde7711d2afb7ce634a48a1522d148
Jan Hoga <janpaulhoga@gmail.com>
2018-08-16 15:41:20 +0900

    landing page form

--- app/models/car.rb
+++ app/models/car.rb
@@ -9,7 +9,7 @@ class Car < ApplicationRecord
 
   include PgSearch
   pg_search_scope :search_by_car,
-    against: [ :brand, :model ],
+    against: [ :brand, :model, :location ],
     using: {
       tsearch: { prefix: true } # <-- now `superman batm` will return something!
     }
