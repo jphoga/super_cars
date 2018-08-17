d165e2a1f8f1460656c24fcc4f9689939775638e
Jan Hoga <janpaulhoga@gmail.com>
2018-08-17 12:14:41 +0900

    search fixed

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -4,9 +4,23 @@ class CarsController < ApplicationController
 
   def index
     skip_policy_scope
-    if params[:query].present?
+    if params[:start].present? && params[:end].present?
+      # cars = Car.search_by_car(params[:query])
+      # # @cars.order("created_at DESC")
+
+      # car_arr = []
+      # cars.each do |car|
+      #   car.bookings.each do |booking|
+      #     if booking.start_date > params[:end] || booking.end_date < params[:start]
+      #       car_arr << car
+      #     end
+      #   end
+      # end
+      # @cars = car_arr.uniq
+      @cars = Car.joins(:bookings).group("cars.id").where("cars.location ILIKE :query AND (bookings.start_date > :end OR bookings.end_date < :start)",
+                                                          query: params[:query], end: params[:end], start: params[:start])
+    elsif params[:query].present?
       @cars = Car.search_by_car(params[:query])
-      # @cars.order("created_at DESC")
     else
       @cars = Car.all
     end
